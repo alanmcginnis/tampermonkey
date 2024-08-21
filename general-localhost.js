5b603821322179dc75b5d898d0678c75a6d161cc
Alan J McGinnis <7727219+alanmcginnis@users.noreply.github.com>
2024-08-21 15:44:57 -0700

    tm scripts

--- general-localhost.js
+++ general-localhost.js
@@ -1,5 +1,5 @@
 (function () {
-  var port = 8080;
+  let port = 8080;
   var scriptElement = document.createElement('script');
   scriptElement.type = 'text/javascript';
   scriptElement.src = `//localhost:${port}/variation.js`;
@@ -10,5 +10,5 @@
   stylesheet.href = `//localhost:${port}/variation.css`;
   document.head.appendChild(stylesheet);
 
-  console.log(`Serving on ${port} from https://raw.githubusercontent.com/alanmcginnis/Scripts/main/TamperMonkey/pm.js?token=GHSAT0AAAAAACVLHXNQL7FQ57MNGE7ZX32GZVEACHQ`)
+  console.log(`🐵 xoptcli on port ${port}.`)
 })();
