66b8232bcaf65e849cd4113dd406e88c50ff25f6
Alan J McGinnis <7727219+alanmcginnis@users.noreply.github.com>
2024-08-28 13:31:28 -0700

    this work

--- general-localhost.meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name         Localhost Injection
-// @description  Injection for xoptcli
-// @author       Alan McGinnis
-// @version      0.1
-// @updateURL    https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.meta.js
-// @downloadURL  https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.user.js
-// ==/UserScript==
--- general-localhost.user.js
+++ general-localhost.user.js
@@ -3,7 +3,7 @@
 // @description  Injection for xoptcli
 // @author       Alan McGinnis
 // @version      0.1
-// @updateURL    https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.meta.js
+// @updateURL    https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.user.js
 // @downloadURL  https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.user.js
 // ==/UserScript==
 
