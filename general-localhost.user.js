e63285afffaaa96a73a540bc15c87418096285fd
Alan J McGinnis <7727219+alanmcginnis@users.noreply.github.com>
2024-08-28 13:36:23 -0700

    test update

--- general-localhost.user.js
+++ general-localhost.user.js
@@ -2,7 +2,7 @@
 // @name         Localhost Injection
 // @description  Injection for xoptcli
 // @author       Alan McGinnis
-// @version      0.1
+// @version      0.1.1
 // @updateURL    https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.user.js
 // @downloadURL  https://raw.githubusercontent.com/alanmcginnis/tampermonkey/main/general-localhost.user.js
 // ==/UserScript==
